95da83996c34802150c701155a4bac8c1a9a883c
Marco de CNK <virtualnautilus@gmail.com>
2025-03-05 21:40:09 -0300

    struggling for deploy

--- front-end/src/pages/LoginPage.tsx
+++ front-end/src/pages/LoginPage.tsx
@@ -1,14 +1,14 @@
-import LoginCard from '@/components/LoginCard'
-import Navbar from '@/components/Navbar'
-import React from 'react'
+// import LoginCard from '@/components/LoginCard'
+// import Navbar from '@/components/Navbar'
+// import React from 'react'
 
-const LoginPage = () => {
-  return (
-    <>
-        <Navbar/>
-        <LoginCard/>
-    </>
-  )
-}
+// const LoginPage = () => {
+//   return (
+//     <>
+//         <Navbar/>
+//         <LoginCard/>
+//     </>
+//   )
+// }
 
-export default LoginPage
+// export default LoginPage
