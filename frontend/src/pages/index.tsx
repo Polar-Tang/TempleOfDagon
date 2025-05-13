d491058a12476dc3ea37d31158c32ff7a3934021
marco <virtualnautilus@gmail.com>
2025-05-13 15:14:05 -0300

    eliminating an unused page cause vite is a faggot that hidde errors that are triggered just in production

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,8 +7,7 @@ import RecoveryPassPage from './auth/RecoveryPassPage'
 import ProductsPage from './productsPages/ProductsPage'
 import ProductDetailPage from "./productsPages/ProductDetailPage"
 import ProductsList from './productsPages/ProductsList'
-import SocketPage from "./SocketPage"
 import Questionnaire from "./Questionnaire"
 import CheckoutPage from "./CheckoutPage"
 
-export { Hell, Home, LoginPage, RegisterPage, ForgotPassPage, RecoveryPassPage, ProductsPage, ProductDetailPage, ProductsList, SocketPage, Questionnaire, CheckoutPage }
+export { Hell, Home, LoginPage, RegisterPage, ForgotPassPage, RecoveryPassPage, ProductsPage, ProductDetailPage, ProductsList, Questionnaire, CheckoutPage }
